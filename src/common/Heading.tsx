737babfff1355657f660e7af1bfec14eab12ec4d
AzarAhmadov <ehmedovh111@gmail.com>
2024-02-05 18:22:05 +0400

    Creating Heading component

---
+++ src/common/Heading.tsx
@@ -0,0 +1,12 @@
+import { Props } from '@/types/Types'
+import React from 'react'
+
+const Heading: React.FC<Props> = ({ title, icon }) => {
+    return (
+        <h4 className='text-white text-[24px] text-center mt-10 flex justify-center border-slate-600 border-b-[1px] pb-2'>
+            {title} {icon}
+        </h4>
+    )
+}
+
+export default Heading

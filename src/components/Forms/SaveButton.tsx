a8fb8125b818bc860f59a44737f5be630b80bc28
AzarAhmadov <ehmedovh111@gmail.com>
2024-02-09 00:38:33 +0400

    Created Save Button component

--- src/components/Forms/SaveButton.tsx
+++ src/components/Forms/SaveButton.tsx
@@ -2,12 +2,19 @@ import { Props } from '@/types/Types'
 import React from 'react'
 
 const SaveButton: React.FC<Props> = ({ title, icon, onClick }) => {
+
+    const refresh = () => {
+        window.location.href = '/Edit';
+    }
+
     return (
-        <button
-            onClick={onClick}
-            className='bg-[#46627b] w-full mt-7 rounded-md p-2 text-white hover:scale-95 transition-all font-medium flex justify-center text-[18px]'>
-            {title} {icon}
-        </button>
+        <div onClick={() => refresh()}>
+            <button
+                onClick={onClick}
+                className='bg-[#46627b] w-full mt-7 rounded-md p-2 text-white hover:scale-95 transition-all font-medium flex justify-center text-[18px]'>
+                {title} {icon}
+            </button>
+        </div>
     );
 };
 

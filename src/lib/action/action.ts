4131a50fd047d1fb9cd53ee939afa9760601feb0
AzarAhmadov <ehmedovh111@gmail.com>
2024-02-10 18:59:11 +0400

    Updated

--- src/lib/action/action.ts
+++ src/lib/action/action.ts
@@ -18,7 +18,7 @@ export const addBio = async (formData: FormData) => {
         });
 
         await newBio.save();
-        revalidatePath('/Demo')
+        revalidatePath('/Edit')
 
     } catch (err) {
         console.log(err);
@@ -50,7 +50,7 @@ export const addSosial = async (FormData: FormData) => {
         })
 
         await newLink.save();
-        revalidatePath('/Demo')
+        revalidatePath('/Edit')
 
     } catch (err) {
         console.log(err)
@@ -96,7 +96,7 @@ export const updateProfile = async (FormData: FormData) => {
         const { id, url } = Object.fromEntries(FormData);
         connectDB()
         await Profile.findOneAndUpdate({ id }, { url }, { new: true });
-        revalidatePath('/')
+        revalidatePath('/Edit')
 
     } catch (err) {
         console.log(err)
@@ -115,7 +115,7 @@ export const addBackProfile = async (FormData: FormData) => {
         })
 
         await newUrl.save();
-        revalidatePath('/')
+        revalidatePath('/Edit')
 
     } catch (err) {
         console.log(err)
@@ -129,7 +129,7 @@ export const updateBackProfile = async (FormData: FormData) => {
         const { id, back_url } = Object.fromEntries(FormData);
         connectDB()
         await ProfileBack.findOneAndUpdate({ id }, { back_url }, { new: true });
-        revalidatePath('/')
+        revalidatePath('/Edit')
 
     } catch (err) {
         console.log(err)

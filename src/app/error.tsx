6366c43fe980a45f3e8efee6270908322921e245
AzarAhmadov <ehmedovh111@gmail.com>
2024-02-10 23:21:49 +0400

    Designed error component

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,13 +1,20 @@
 'use client'
 
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
-const error: React.FC = () => {
+const Error: React.FC = () => {
     return (
-        <div>
-            Error
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h3 className="text-3xl font-bold mb-4">Error</h3>
+            <p className="text-gray-600 mb-4 text-center">An error occurred while processing your request.</p>
+            <Link href='/Demo'
+                className="bg-[#000] hover:bg-[#00000097] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+                Go Back
+            </Link>
         </div>
-    )
-}
+    );
+};
 
-export default error
+export default Error;

8fd4eff72ea46dfcb4eb8039d28190f931ceb817
AzarAhmadov <ehmedovh111@gmail.com>
2024-02-10 23:21:32 +0400

    Designed loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const loading: React.FC = () => {
     return (
-        <div>
-            Loading....
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-white opacity-75 z-50">
+            <div className="animate-spin rounded-full h-20 w-20 border-b-2 border-gray-900"></div>
         </div>
     )
 }

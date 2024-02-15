3b4829719016e44d8430b8c3a10f808ffe45795c
AzarAhmadov <ehmedovh111@gmail.com>
2024-02-16 00:18:02 +0400

    Updated

--- src/common/Heading.tsx
+++ src/common/Heading.tsx
@@ -1,7 +1,7 @@
-import { Props } from '@/types/Types'
+import { HeadingProps } from '@/types/Types'
 import React from 'react'
 
-const Heading: React.FC<Props> = ({ title, icon }) => {
+const Heading: React.FC<HeadingProps> = ({ title, icon }) => {
     return (
         <h4 className='text-white text-[24px] text-center mt-10 flex justify-center border-slate-600 border-b-[1px] pb-2'>
             {title} {icon}

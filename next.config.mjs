269df85ee1baec0259df5086dfed45ed2140f4dc
AzarAhmadov <ehmedovh111@gmail.com>
2024-02-17 14:05:04 +0400

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects: async () => {
+        return [{
+            source: '/',
+            destination: '/AzarAhmadov',
+            permanent: true,
+        }, ];
+    },
+};
 
 export default nextConfig;
